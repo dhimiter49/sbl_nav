f98844371acbec62cadeaaac317f6f84afa1a8d4
dhimiter <dhimpik@proton.me>
2024-03-06 23:37:20 +0100

    reimplement test case

--- ppo.py
+++ ppo.py
@@ -29,27 +29,29 @@ tb_path = tb_time() if "-p" not in sys.argv else tb_custom()
 tb_path = "exp/" + tb_path
 test = "-t" in sys.argv
 n_envs = 8 if "-ne" not in sys.argv else int(num_env())
-n_envs = 1 if test else n_envs
 
-
-vec_env = make_vec_env(env_id, n_envs=n_envs)
 if test:
     model = PPO.load(load_path)
 
-    obs = vec_env.reset()
-    while True:
+    env = gym.make(env_id)
+    obs, _ = env.reset()
+    for i in range(10000):
         action, _ = model.predict(obs)
-        obs, rewards, dones, info = vec_env.step(action)
-        vec_env.render("human")
-else:
+        obs, rewards, dones, terminal, info = env.step(action)
+        env.render()
 
+        if dones or terminal:
+            obs, _ = env.reset()
+else:
+    vec_env = make_vec_env(env_id, n_envs=n_envs)
     if load_path is not None:
         model = PPO.load(load_path, env=vec_env)
     else:
         model = PPO(
             "MlpPolicy", vec_env,
             policy_kwargs={"net_arch": {"pi": [256, 256], "vf": [256, 256]}},
-            clip_range=0.15,
+            clip_range=0.2,
+            clip_range_vf=0.2,
             learning_rate=1e-4,
             verbose=1,
             n_steps=64,
